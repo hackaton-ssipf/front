32caaf10d26c8362848213b6ab260d0e0d1a176c
andrewzn <ondrejzeman23@gmail.com>
2023-05-16 17:03:37 +0200

    feat(theme.js): add colours
    TODO: CHANGE FONTS

--- src/theme.js
+++ src/theme.js
@@ -82,9 +82,6 @@ export const themeSettings = mode => {
   const colors = tokens(mode)
   return {
     palette: {
-      mode: mode,
-      ...(mode === 'dark'
-        ? {
             // palette values for dark mode
             primary: {
               main: colors.primary[500]
@@ -101,24 +98,7 @@ export const themeSettings = mode => {
               default: colors.primary[500]
             }
           }
-        : {
-            // palette values for light mode
-            primary: {
-              main: colors.primary[100]
-            },
-            secondary: {
-              main: colors.greenAccent[500]
-            },
-            neutral: {
-              dark: colors.grey[700],
-              main: colors.grey[500],
-              light: colors.grey[100]
-            },
-            background: {
-              default: '#fcfcfc'
-            }
-          })
-    },
+    };
     typography: {
       fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
       fontSize: 12,
